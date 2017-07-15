34554f27e67b7c56f71fce182942d14e44348c87
Nick Ramsay <nickramsay19@gmail.com>
2017-07-15 17:32:05 +1000

    added sidebar selections

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 $(document).ready(function(){
     $(".link-page-home").click(function(){
-        HideAllPages();
+        ResetPageSelection();
         setTimeout(function(){
+            $(".link-page-home").addClass("is-active");
             $("#page-home").fadeIn(500, function(){});
         },
         200);
 
     });
     $(".link-page-heroes").click(function(){
-        HideAllPages();
+        ResetPageSelection();
         setTimeout(function(){
+            $(".link-page-heroes").addClass("is-active");
             $("#page-heroes").fadeIn(500, function(){});
         },
         200);
     });
     $(".link-page-podcasts").click(function(){
-        HideAllPages();
+        ResetPageSelection();
         setTimeout(function(){
+            $(".link-page-podcasts").addClass("is-active");
             $("#page-podcasts").fadeIn(500, function(){});
         },
         200);
     });
     $(".link-page-viper").click(function(){
-        HideAllPages();
+        ResetPageSelection();
         setTimeout(function(){
+            $(".link-page-viper").addClass("is-active");
             $("#page-viper").fadeIn(500, function(){});
         },
         200);
@@ -31,9 +35,17 @@ $(document).ready(function(){
 });
 
 
-function HideAllPages(){
+function ResetPageSelection(){
+
+    //Hide All Pages
     $("#page-home").fadeOut(100, function(){});
     $("#page-podcasts").fadeOut(100, function(){});
     $("#page-heroes").fadeOut(100, function(){});
     $("#page-viper").fadeOut(100, function(){});
+
+    //Clear SideBar Styles
+    $(".link-page-home").removeClass("is-active");
+    $(".link-page-podcasts").removeClass("is-active");
+    $(".link-page-heroes").removeClass("is-active");
+    $(".link-page-viper").removeClass("is-active");
 }
