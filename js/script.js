c9d4b43a128b887115d9f5bfc8eeb77b8b34f738
Nick Ramsay <nickramsay19@gmail.com>
2017-07-15 20:42:05 +1000

    added breadcrumbs and all pages including hero pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Page Links
 $(document).ready(function(){
     $(".link-page-home").click(function(){
         ResetPageSelection();
@@ -38,6 +39,86 @@ $(document).ready(function(){
             $(".link-page-viper-viper").addClass("is-active");
             $("#page-viper-viper").fadeIn(500, function(){});
         },
+        200);
+    });
+    $(".link-page-viper-jp").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-viper-jp").addClass("is-active");
+            $("#page-viper-jp").fadeIn(500, function(){});
+        },
+        200);
+    });
+    $(".link-page-spectre").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-spectre").addClass("is-active");
+            $("#page-spectre").fadeIn(500, function(){});
+        },
+        200);
+    });
+    $(".link-page-mockingbird").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-mockingbird").addClass("is-active");
+            $("#page-mockingbird").fadeIn(500, function(){});
+        },
+                   200);
+    });
+    $(".link-page-burning").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-burning").addClass("is-active");
+            $("#page-burning").fadeIn(500, function(){});
+        },
+                   200);
+    });
+    $(".link-page-jack").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-jack").addClass("is-active");
+            $("#page-jack").fadeIn(500, function(){});
+        },
+                   200);
+    });
+    $(".link-page-dilhan").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-dilhan").addClass("is-active");
+            $("#page-dilhan").fadeIn(500, function(){});
+        },
+                   200);
+    });
+    $(".link-page-hannah").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-hannah").addClass("is-active");
+            $("#page-hannah").fadeIn(500, function(){});
+        },
+                   200);
+    });
+    $(".link-page-tom").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-tom").addClass("is-active");
+            $("#page-tom").fadeIn(500, function(){});
+        },
+                   200);
+    });
+    $(".link-page-miranda").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-miranda").addClass("is-active");
+            $("#page-miranda").fadeIn(500, function(){});
+        },
+                   200);
+    });
+    $(".link-page-hunter").click(function(){
+        ResetPageSelection();
+        setTimeout(function(){
+            $(".link-page-hunter").addClass("is-active");
+            $("#page-hunter").fadeIn(500, function(){});
+        },
                    200);
     });
     $(".link-page-team").click(function(){
@@ -66,7 +147,7 @@ $(document).ready(function(){
     });
 });
 
-
+//Reset All Pages & Page Links
 function ResetPageSelection(){
 
     //Hide All Pages
@@ -75,6 +156,16 @@ function ResetPageSelection(){
     $("#page-heroes").fadeOut(100, function(){});
     $("#page-viper").fadeOut(100, function(){});
     $("#page-viper-viper").fadeOut(100, function(){});
+    $("#page-viper-jp").fadeOut(100, function(){});
+    $("#page-spectre").fadeOut(100, function(){});
+    $("#page-mockingbird").fadeOut(100, function(){});
+    $("#page-burning").fadeOut(100, function(){});
+    $("#page-jack").fadeOut(100, function(){});
+    $("#page-dilhan").fadeOut(100, function(){});
+    $("#page-hannah").fadeOut(100, function(){});
+    $("#page-tom").fadeOut(100, function(){});
+    $("#page-miranda").fadeOut(100, function(){});
+    $("#page-hunter").fadeOut(100, function(){});
     $("#page-team").fadeOut(100, function(){});
     $("#page-discover").fadeOut(100, function(){});
     $("#page-timeline").fadeOut(100, function(){});
@@ -83,7 +174,6 @@ function ResetPageSelection(){
     $(".link-page-home").removeClass("is-active");
     $(".link-page-podcasts").removeClass("is-active");
     $(".link-page-heroes").removeClass("is-active");
-    $(".link-page-viper").removeClass("is-active");
     $(".link-page-team").removeClass("is-active");
     $(".link-page-discover").removeClass("is-active");
     $(".link-page-timeline").removeClass("is-active");
