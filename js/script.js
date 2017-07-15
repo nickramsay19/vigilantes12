cd1e446e79c03964cedb6f5112e2fdeeb8dafa8b
Nick Ramsay <nickramsay19@gmail.com>
2017-07-15 16:16:10 +1000

    Added viper page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#side-nav-home").click(function(){
+    $("#link-page-home").click(function(){
         HideAllPages();
         setTimeout(function(){
             $("#page-home").fadeIn(500, function(){});
@@ -7,17 +7,24 @@ $(document).ready(function(){
         200);
 
     });
-    $("#side-nav-heroes").click(function(){
+    $("#link-page-heroes").click(function(){
         HideAllPages();
         setTimeout(function(){
             $("#page-heroes").fadeIn(500, function(){});
         },
         200);
     });
-    $("#side-nav-podcasts").click(function(){
+    $("#link-page-podcasts").click(function(){
         HideAllPages();
         setTimeout(function(){
             $("#page-podcasts").fadeIn(500, function(){});
+        },
+        200);
+    });
+    $("#link-page-viper").click(function(){
+        HideAllPages();
+        setTimeout(function(){
+            $("#page-viper").fadeIn(500, function(){});
         },
         200);
     });
@@ -28,4 +35,5 @@ function HideAllPages(){
     $("#page-home").fadeOut(100, function(){});
     $("#page-podcasts").fadeOut(100, function(){});
     $("#page-heroes").fadeOut(100, function(){});
+    $("#page-viper").fadeOut(100, function(){});
 }
